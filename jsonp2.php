a22f1c0685142b65338e48cf1d9ae78be8072620
zhidelin <zhidelin@qq.com>
2014-06-05 19:21:51 +0800

    Create jsonp2.php
    service

---
+++ jsonp2.php
@@ -0,0 +1,9 @@
+<?php
+	
+		if($_GET["callback"]){
+
+		echo $_GET["callback"].'('.'{"AS":{"Query":"linsaaattttaaa","FullResults":1,"Results":[{"Type":"AS","Suggests":[{"Txt":"林志颖","Type":"AS","Sk":"","HCS":0.336499},{"Txt":"林正英","Type":"AS","Sk":"AS1"},{"Txt":"林心如","Type":"AS","Sk":"AS2"},{"Txt":"林志玲","Type":"AS","Sk":"AS3"},{"Txt":"林志颖老婆陈若仪","Type":"AS","Sk":"AS4"},{"Txt":"林允儿","Type":"AS","Sk":"AS5"},{"Txt":"林峰","Type":"AS","Sk":"AS6"},{"Txt":"林正英僵尸电影全集","Type":"AS","Sk":"AS7"}]}]}}'.')';
+
+		}
+
+?>
